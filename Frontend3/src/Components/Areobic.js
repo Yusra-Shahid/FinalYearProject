5dcaa8ea180918b4cceac33c1e991e57246eb2b3
yusra <yusrashahid111@gmail.com>
2022-05-12 11:48:21 +0500

    mid work complete

--- Frontend3/src/Components/Areobic.js
+++ Frontend3/src/Components/Areobic.js
@@ -11,14 +11,14 @@ import Typography from '@mui/material/Typography';
 import ExMainPage from "./ExMainPage";
 import ImgMediaCard from "./ButtonComponent/View";
  import Aero1 from "../Images/Aero1.mp4";
-//  import stre2 from "../Images/stre2.mp4";
+  import areo2 from "../Images/areo2.mp4";
  import videoBg from "../Images/videoBg.mp4";
 
 
 
     const Videosrc0= videoBg
     const Videosrc1 = Aero1;
-    // const Videosrc2 = stre2;
+    const Videosrc2 = areo2;
 
 export class Areobic extends Component {
 
@@ -42,30 +42,29 @@ export class Areobic extends Component {
                 <div className="Gendercard">
                 <div className=" card_Container1 ">
                     <div className='malercard'>
-                        <Card sx={{ maxWidth: 300 }}>
-                            <CardMedia
-                                component="img"
-                                alt="green iguana"
-                                height="200"
-                                image="https://image.shutterstock.com/image-photo/young-man-sportswear-running-on-260nw-428663329.jpg"
-                            />
-                            <CardContent>
-                                <Typography gutterBottom variant="h5" component="div">
-                                    Male Exersice
-                                </Typography>
-                                <Typography variant="body2" color="text.secondary"> across all continents except Antarctica
-                                </Typography>
-                            </CardContent>
+                        <Card sx={{ maxWidth: 490 }}>
+                        <div>
+                                    <video src={areo2} muted="true" autoPlay="true" controls="controls"/>
+                                    </div>
+                    
                         </Card>
                     </div>
+                    <div className="exersice-text">
+                            <p>1. Start by jogging forward as you swing the jump rope over your head and under your feet.<br></br>
+                                2. Next, reverse your direction and jog backward as you continue to swing the jump rope.<br></br>
+                                 3. Finish your set by doing a hopscotch jump for 15 seconds. 4. Rest for 15 seconds between sets.
+                                <br></br> 5. Repeat 18 times.</p>
+                            </div> 
                     </div>
                     <div>
                         <div className="card_Container2 ">
-                            <div className="exersice-text"></div>
-                            {/* <p>1. Start by jogging forward as you swing the jump rope over your head and under your feet.<br></br>
+                            <div className="exersice-text">
+                            <p>1. Start by jogging forward as you swing the jump rope over your head and under your feet.<br></br>
                                 2. Next, reverse your direction and jog backward as you continue to swing the jump rope.<br></br>
                                  3. Finish your set by doing a hopscotch jump for 15 seconds. 4. Rest for 15 seconds between sets.
-                                <br></br> 5. Repeat 18 times.</p> */}
+                                <br></br> 5. Repeat 18 times.</p>
+                            </div>
+                           
                         <div className='Femalercard'>
                             <Card sx={{ maxWidth: 490 }}>
                                 <div>
--- Frontend3/src/Components/Stretching.js
+++ Frontend3/src/Components/Stretching.js
@@ -12,6 +12,8 @@ import Typography from '@mui/material/Typography';
 import ExMainPage from "./ExMainPage";
 import ImgMediaCard from "./ButtonComponent/View";
  import videoBg from "../Images/videoBg.mp4";
+ import stre1 from "../Images/stre1.mp4";
+ import stre2 from "../Images/stre2.mp4";
 
 
  
@@ -33,47 +35,38 @@ export class Stretching extends Component {
                 </div>
 
                 <div className="Stretching-video">
-                    <video src={videoBg} autoPlay="true" controls="controls"/>
+                    <video src={videoBg} muted="true" autoPlay="true" controls="controls"/>
                     </div>
                 <diV className="Sec-Context">Discover workouts that will fit your mood, goals, fitness level, music taste & schedule</diV>
                 <div className="Gendercard">
                 <div className=" card_Container1 ">
+
                     <div className='malercard'>
-                        <Card sx={{ maxWidth: 300 }}>
-                            <CardMedia
-                                component="img"
-                                alt="green iguana"
-                                height="180"
-                                image="https://image.shutterstock.com/image-photo/young-man-sportswear-running-on-260nw-428663329.jpg"
-                            />
-                            <CardContent>
-                                <Typography gutterBottom variant="h5" component="div">
-                                    Male Exersice
-                                </Typography>
-                                <Typography variant="body2" color="text.secondary"> across all continents except Antarctica
-                                </Typography>
-                            </CardContent>
+                        
+                        <Card sx={{ maxWidth: 450 }}>
+                        <div>
+                
+                <video src={stre1} muted="true" autoPlay="true" controls="controls"/>
+                </div>
+                    
                         </Card>
+                    </div>
+                    <div className="stre-text">
+                        <p>Sit on the floor or a mat and bring your feet together so that your soles touch and your knees bend to opposite sides. With a straight spine, grasp your feet, then lean slowly forward and gently push your thighs down with your elbows until you feel the stretch along your inner thighs. Hold for 15 to 30 seconds.</p>
                     </div>
                     </div>
                     <div>
                         <div className="card_Container2 ">
+                        <div className="stre-text">
+                        <p>Sit on the floor or a mat and bring your feet together so that your soles touch and your knees bend to opposite sides. With a straight spine, grasp your feet, then lean slowly forward and gently push your thighs down with your elbows until you feel the stretch along your inner thighs. Hold for 15 to 30 seconds.</p>
+                    </div>
                         <div className='Femalercard'>
-                            <Card sx={{ maxWidth: 300 }}>
+                            <Card sx={{ maxWidth: 450 }}>
                                 <div>
-                                    {/* // component="img"
-                                    // alt="green iguana"
-                                    // height="180"
-                                    // image="https://images.unsplash.com/photo-1485727749690-d091e8284ef3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHdvbWFuJTIwd29ya291dHxlbnwwfHwwfHw%3D&w=1000&q=80" */}
-                                    <video src={videoBg} muted="true" autoPlay="true" controls="controls"/>
+                
+                                    <video src={stre2} muted="true" autoPlay="true" controls="controls"/>
                                     </div>
-                                <CardContent>
-                                    <Typography gutterBottom variant="h5" component="div">
-                                        Female Exersice
-                                    </Typography>
-                                    <Typography variant="body2" color="text.secondary"> across all continents except Antarctica
-                                    </Typography>
-                                </CardContent>
+                               
 
                             </Card>
                         </div>
