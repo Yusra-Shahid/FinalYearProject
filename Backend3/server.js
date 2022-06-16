946acc43119fb73dd31895d4aeb9878f83b099aa
yusra <yusrashahid111@gmail.com>
2022-06-17 00:07:21 +0500

    css

--- Backend3/server.js
+++ Backend3/server.js
@@ -66,22 +66,22 @@ const io = require("socket.io")(server, {
 		methods: [ "GET", "POST" ]
 	}
 });
-// io.on("connection", (socket) => {
-// 	socket.emit("me", socket.id);
+io.on("connection", (socket) => {
+	socket.emit("me", socket.id);
 
-// 	socket.on("disconnect", () => {
-// 		socket.broadcast.emit("callEnded")
-// 	});
+	socket.on("disconnect", () => {
+		socket.broadcast.emit("callEnded")
+	});
 
-// 	socket.on("callUser", ({ userToCall, signalData, from, name }) => {
-// 		io.to(userToCall).emit("callUser", { signal: signalData, from, name });
-// 	});
+	socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+		io.to(userToCall).emit("callUser", { signal: signalData, from, name });
+	});
 
-// 	socket.on("answerCall", (data) => {
-// 		io.to(data.to).emit("callAccepted", data.signal)
-// 	});
-// });
+	socket.on("answerCall", (data) => {
+		io.to(data.to).emit("callAccepted", data.signal)
+	});
+});
 // const PORT = 5000;
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 // server.listen(port, () => console.log(`listening port ${port}`));
